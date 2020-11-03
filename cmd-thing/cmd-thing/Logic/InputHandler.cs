318950df00ecf0b46a8d254bfd6b72919ec6c76c
salt <2plusde@gmail.com>
2020-11-03 13:51:51 +0100

    doesn't say "hello world" anymore.

---
+++ cmd-thing/cmd-thing/Logic/InputHandler.cs
@@ -0,0 +1,10 @@
+﻿using cmd_thing.Objects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cmd_thing.Logic {
+    class InputHandler {
+
+    }
+}
--- cmd-thing/cmd-thing/Program.cs
+++ cmd-thing/cmd-thing/Program.cs
@@ -1,9 +1,26 @@
-﻿using System;
+﻿using cmd_thing.Logic;
+using System;
 
 namespace cmd_thing {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            // I'm lazy
+            Action<String> cw = Console.Write;
+            Action<String> cwl = Console.WriteLine;
+
+            // read inputs
+            Console.ForegroundColor = ConsoleColor.Gray;
+            cw("Height: ");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            String input1 = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Gray;
+            cw("Width: ");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            String input2 = Console.ReadLine();
+
+            // send to inputhandler
+
+            // stop
             Console.ReadKey();
         }
     }
